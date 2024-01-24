c289a00b1e868927a748e6de4953b88040ff8c5b
SandyMrtss <bebesubi2@hotmail.com>
2024-01-24 11:59:12 +0100

    Formats dto

--- src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s05/t03/model/dto/PlayerDTO.java
+++ src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s05/t03/model/dto/PlayerDTO.java
@@ -1,4 +1,20 @@
 package cat.itacademy.barcelonactiva.martos.sandra.s05.t03.model.dto;
 
+import lombok.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
 public class PlayerDTO {
+    private String username;
+
+    private double successRate;
+
+    public PlayerDTO(String username, double successRate) {
+        this.username = Objects.requireNonNullElse(username, "ANONYMUS");
+        this.successRate = successRate;
+    }
+
 }
---
+++ src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s05/t03/model/dto/Request/PlayerDTORequest.java
@@ -0,0 +1,8 @@
+package cat.itacademy.barcelonactiva.martos.sandra.s05.t03.model.dto.Request;
+
+import lombok.*;
+@Getter
+@Setter
+public class PlayerDTORequest {
+    private String username;
+}
