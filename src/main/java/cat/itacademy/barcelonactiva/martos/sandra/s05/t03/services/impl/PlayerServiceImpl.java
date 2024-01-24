1247ac5d7e9c279bebc5c6b2fe3a7ff97d82d1f9
SandyMrtss <bebesubi2@hotmail.com>
2024-01-24 12:00:26 +0100

    Implements player service

--- src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s05/t03/services/impl/PlayerServiceImpl.java
+++ src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s05/t03/services/impl/PlayerServiceImpl.java
@@ -1,8 +1,87 @@
 package cat.itacademy.barcelonactiva.martos.sandra.s05.t03.services.impl;
 
+import cat.itacademy.barcelonactiva.martos.sandra.s05.t03.model.domain.PlayerEntity;
+import cat.itacademy.barcelonactiva.martos.sandra.s05.t03.model.dto.GameDTO;
+import cat.itacademy.barcelonactiva.martos.sandra.s05.t03.model.dto.PlayerDTO;
+import cat.itacademy.barcelonactiva.martos.sandra.s05.t03.model.dto.Request.PlayerDTORequest;
+import cat.itacademy.barcelonactiva.martos.sandra.s05.t03.repository.PlayerRepository;
+import cat.itacademy.barcelonactiva.martos.sandra.s05.t03.services.GameService;
 import cat.itacademy.barcelonactiva.martos.sandra.s05.t03.services.PlayerService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerServiceImpl implements PlayerService {
+    PlayerRepository playerRepository;
+    GameService gameService;
+    public PlayerServiceImpl(PlayerRepository playerRepository, GameService gameService){
+        this.playerRepository = playerRepository;
+        this.gameService = gameService;
+    }
+    @Override
+    public void addPlayer(PlayerDTORequest playerDTORequest) {
+        playerRepository.save(playerDTOToEntity(playerDTORequest));
+    }
+
+    @Override
+    public void updatePlayer(Integer id, PlayerDTORequest playerDTORequest) {
+        Optional<PlayerEntity> playerEntity = playerRepository.findById(id);
+        if(playerEntity.isEmpty()){
+            throw new EntityNotFoundException();
+        }
+        playerEntity.get().setUsername(playerDTORequest.getUsername());
+        playerRepository.save(playerEntity.get());
+    }
+
+    @Override
+    public PlayerEntity getPlayer(Integer id) {
+        Optional<PlayerEntity> playerEntity = playerRepository.findById(id);
+        if(playerEntity.isEmpty()){
+            throw new EntityNotFoundException();
+        }
+        return playerEntity.get();
+    }
+
+    @Override
+    public GameDTO addGame(Integer idPlayer) {
+        PlayerEntity playerEntity = getPlayer(idPlayer);
+        return gameService.addGame(playerEntity);
+    }
+
+    @Override
+    public List<GameDTO> getAllGames(Integer idPlayer) {
+        PlayerEntity playerEntity = getPlayer(idPlayer);
+        return gameService.getAllGames(playerEntity);
+    }
+
+    @Override
+    public void deleteAllGames(Integer idPlayer) {
+        PlayerEntity playerEntity = getPlayer(idPlayer);
+        gameService.deleteAllGames(playerEntity);
+    }
+
+    @Override
+    public List<PlayerEntity> getAllPlayers(){
+        return playerRepository.findAll();
+    }
+    @Override
+    public List<PlayerDTO> getSuccessRate() {
+        List<PlayerEntity> playerEntityList = getAllPlayers();
+        List<PlayerDTO> playerDTOList = new ArrayList<>();
+        playerEntityList.forEach(p-> playerDTOList.add(new PlayerDTO(p.getUsername(), gameService.getSuccessRate(p))));
+        return playerDTOList;
+    }
+
+    private PlayerDTO playerToDTO(PlayerEntity playerEntity){
+        double successRate = gameService.getSuccessRate(playerEntity);
+        return new PlayerDTO(playerEntity.getUsername(), successRate);
+    }
+
+    private PlayerEntity playerDTOToEntity(PlayerDTORequest playerDTORequest){
+        return new PlayerEntity(playerDTORequest.getUsername());
+    }
 }
