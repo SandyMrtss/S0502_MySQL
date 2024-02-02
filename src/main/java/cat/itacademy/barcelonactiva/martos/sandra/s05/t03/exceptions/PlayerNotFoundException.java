ad89f138ffa5efa0cc06574df554b4b1bcbd7649
Sandy <bebesubi2@hotmail.com>
2024-02-02 11:35:47 +0100

    repo reformat

--- src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s05/t03/exceptions/PlayerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.martos.sandra.s05.t03.exceptions;
-
-import jakarta.persistence.EntityNotFoundException;
-
-public class PlayerNotFoundException extends EntityNotFoundException {
-    public PlayerNotFoundException(){
-        super("No player was found with the chosen id");
-    }
-}
